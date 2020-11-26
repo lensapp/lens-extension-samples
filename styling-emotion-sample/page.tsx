87721b2909a30f6a5ff9d681b80395716e98df8b
Alex Andreev <alex.andreev.email@gmail.com>
2020-11-26 11:20:43 +0300

    Catching active lens theme
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- styling-emotion-sample/page.tsx
+++ styling-emotion-sample/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import path from "path";
 import styled from "@emotion/styled";
-import { LensRendererExtension, Component } from "@k8slens/extensions";
+import { observer } from "mobx-react";
+import { LensRendererExtension, Component, Theme } from "@k8slens/extensions";
 import { CoffeeDoodle } from "react-open-doodles";
 
 const Wrapper = styled.div`
@@ -24,6 +25,7 @@ export function ExampleIcon(props: Component.IconProps) {
   return <Component.Icon {...props} material="pages" tooltip={path.basename(__filename)}/>
 }
 
+@observer
 export class ExamplePage extends React.Component<{ extension: LensRendererExtension }> {
   render() {
     return (
@@ -31,6 +33,7 @@ export class ExamplePage extends React.Component<{ extension: LensRendererExtens
         <div className="doodle-style"><CoffeeDoodle accent="#3d90ce" /></div>
         <p>Hello world with Emotion!</p>
         <p>File: <i>{__filename}</i></p>
+        <p>Active theme is {Theme.getActiveTheme().name}</p>
       </Wrapper>
     );
   }
